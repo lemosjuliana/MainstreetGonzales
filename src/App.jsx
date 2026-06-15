6243d93d5febe6483e599e440b4071fa76024fff
Juliana Lemos <lemos.julianajesus@gmail.com>
2026-06-15 17:32:51 -0500

    changing browser router to hashrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from "./pages/Home"
 import Businesses from "./pages/Businesses"
 import BusinessDetailsPage from "./pages/BusinessDetails"
